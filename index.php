5ab77d9c527a18bcbf38d06d8424f237ddac9277
Rafael Françozo <rvfrancozo@gmail.com>
2022-08-17 22:42:20 -0400

    IFMSdsd

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
 <?php
 phpinfo();
+
+$db = new PDO('
+pgsql:host=;
+port=;
+dbname=;
+user=;
+password=
+');
+
+$sql ='CREATE TABLE IF NOT EXISTS data (id serial PRIMARY KEY, nome VARCHAR(50) NOT NULL);';
+$db->exec($sql);
+
+$nomes = array('izabel', 'fani', 'vitor', 'carla', 'luis');
+
+print_r($nomes);
+
 ?>
