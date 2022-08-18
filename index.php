35e5e3ae365ca06963abcc6fc5a42bd87749eb4f
Rafael Françozo <rvfrancozo@gmail.com>
2022-08-17 22:58:28 -0400

    IFMSdsd

--- index.php
+++ index.php
@@ -1,12 +1,6 @@
 <?php
 
-$db = pg_connect('
-pgsql:host=ec2-52-207-15-147.compute-1.amazonaws.com
- port=5432
- dbname=d7lvjeb4h7lqu6
- user=osfcoeysyvqoum
- password=5320f06bed143660c30634dafc2a78570a1f0423e9cf540615e543e95ba41062
-');
+$db = pg_connect("host=ec2-52-207-15-147.compute-1.amazonaws.com port=5432 dbname=d7lvjeb4h7lqu6 user=osfcoeysyvqoum password=5320f06bed143660c30634dafc2a78570a1f0423e9cf540615e543e95ba41062");
 
 if($db) {
     echo "<hr>Conectado!<hr>";
