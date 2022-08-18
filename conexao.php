a0e61d81a428a24b465b2fc426aec291a3b8a268
Vitao <victorbenevides2002@gmail.com>
2022-08-18 09:28:26 -0500

    Editando a conexao

--- conexao.php
+++ conexao.php
@@ -1,9 +1,5 @@
 <?php
-$servername = "ec2-44-207-126-176.compute-1.amazonaws.com";
-$port = "5432";
-$database = "dck5m8gitcdan0";
-$username = "iasqejvhrjplfr";
-$password = "1e0dc8cdf49de28794c2a360af6be747bed413729d776374fe513a09e2d16476";
+
 // Create connection
 $conn = pg_connect("host=ec2-44-207-126-176.compute-1.amazonaws.com port=5432 dbname=dck5m8gitcdan0 user=iasqejvhrjplfr password=1e0dc8cdf49de28794c2a360af6be747bed413729d776374fe513a09e2d16476");
 // Check connection
