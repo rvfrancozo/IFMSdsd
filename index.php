103df6babd57b804837116714da40ef640e7fdab
Rafael Françozo <rvfrancozo@gmail.com>
2022-08-17 22:11:42 -0400

    IFMSdsd

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
-phpinfo();
+//phpinfo();
+
+$bdcon = pg_connect("
+host=ec2-52-207-15-147.compute-1.amazonaws.com 
+port=5432 
+dbname=d7lvjeb4h7lqu6 
+user=osfcoeysyvqoum 
+password=5320f06bed143660c30634dafc2a78570a1f0423e9cf540615e543e95ba41062
+");
+
+if($bdcon) {
+    echo "Conectado com sucesso!";
+}
+
+
 ?>
